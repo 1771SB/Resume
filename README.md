16ce73e25ced0d44072dddfcb14046c4bcf5d63c
1771SB <146355065+bridstep@users.noreply.github.com>
2023-11-12 23:10:33 -0500

    readme insert

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-#Version 1
-##Vanilla javascript and CSS
-<img src="assets/Screenshots/image.png" width="250"><img src="assets/Screenshots/image-1.png" width="250">
+#Version 1<br>
+##Vanilla javascript and CSS<br>
+<img src="assets/Screenshots/image.png" width="250"><br>
+<img src="assets/Screenshots/image-1.png" width="250"><br>
 
-#Version 2
-##Added mobile view
+#Version 2<br>
+##Added mobile view<br>
 
 
--- logic.js
+++ logic.js
@@ -72,3 +72,23 @@ function displayContent(event, contentNameID) {
     }
 
 }
+
+function actionButton() {
+
+    document.getElementById("Horizontal").classList.toggle("visible")
+    document.getElementById("Vertical").classList.toggle("visible")
+    
+}
+
+function tabButton(tab) {
+
+    var tabs = ["certTab","projectTab","resumeTab"]
+
+    for (t of tabs) {
+
+        document.getElementById(t).style.display = "none"
+    }
+
+    document.getElementById(tab).style.display = "block"
+
+}
